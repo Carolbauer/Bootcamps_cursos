7e3e207f10ce0178ef1ae0bfff235a8b42eb64a4
carolbauer <99449590+Carolbauer@users.noreply.github.com>
2024-08-27 07:44:20 -0300

    string and boolean

---
+++ cursos_práticas/JavaScript_basico_ao_avancado/5_string_avancado/script.js
@@ -0,0 +1,5 @@
+console.log("Primeira linha /n Segunda linha");
+console.log("Meu nome é Caroline");
+console.log("O nome dela é Roberta");
+console.log(`A multiplicaçao de 5 por 3 é ${5*3}`);
+console.log("o" + "meu" + "nome" + "é" + "caroline");
